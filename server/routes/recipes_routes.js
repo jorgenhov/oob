ea4816c28798833f7093da5292d18b1401315b78
jorgenhov <jorhov19@hotmail.com>
2019-03-18 18:21:13 +0100

    added gitignore

--- server/routes/recipes_routes.js
+++ server/routes/recipes_routes.js
@@ -5,6 +5,7 @@ module.exports = function(app, db) {
   app.post('/recipe/add', (req, res) => {
     const recipe = req.body;
     if(recipe.title && recipe.field1 && recipe.desc) {
+      //change collection!
       db.collection('test').insertOne(recipe, (err, results) => {
         if(err){
           res.send({'error': 'An error occurred'});
